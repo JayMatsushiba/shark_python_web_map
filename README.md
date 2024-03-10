e36a8861688ef0c4fe3848fa59d2212e82fa5e0a
Jay Matsushiba <jay.matsushiba@gmail.com>
2024-03-09 17:48:23 -0800

    init flask app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shark_python_web_map
 Creating a flask web application to show shark ranges
+
+Following this series: https://youtube.com/playlist?list=PL8ApLBFiTZlwpTfOGF6LGJc61IVekKa3s&si=O8PwBfG8-Iw2DMgJ 
---
+++ app.py
@@ -0,0 +1,15 @@
+# Uses request.args.get
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = request.args.get("name", "world")
+    return render_template("index.html", name=name)
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
